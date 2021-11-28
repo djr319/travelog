3f73f85ebadf94c46d10fd57f23e6700b1194656
Eloy <eloy.robillard@gmail.com>
2021-11-28 12:10:11 +0100

    [Client | NavBar] add unit tests

--- client/src/Components/NavBar/NavBar.test.tsx
+++ client/src/Components/NavBar/NavBar.test.tsx
@@ -1,1 +1,37 @@
+import { render, waitFor } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import NavBar from './NavBar';
+
+describe('Navigation', () => {
+	test('displays items on hover', async () => {
+		const { getByText, getAllByRole } = render(
+			<Router>
+				<NavBar />
+			</Router>
+		);
+
+		userEvent.hover(getByText('Navigation'));
+		await waitFor(() => getAllByRole('link'));
+
+		expect(getAllByRole('link')).not.toHaveLength(0);
+	});
+
+  test('contains links to the correct pages', async () => {
+		const { getByText, getAllByRole } = render(
+			<Router>
+				<NavBar />
+			</Router>
+		);
+
+		userEvent.hover(getByText('Navigation'));
+		await waitFor(() => getAllByRole('link'));
+
+		expect(getByText('Dashboard')).toHaveAttribute('href', '/');
+		expect(getByText('Profile')).toHaveAttribute('href', '/profile');
+		expect(getByText('Trips')).toHaveAttribute('href', '/trips');
+		expect(getByText('Journal')).toHaveAttribute('href', '/journal');
+		expect(getByText('Notes')).toHaveAttribute('href', '/notes');
+		expect(getByText('Logout')).toHaveAttribute('href', '/logout');
+	});
+});
