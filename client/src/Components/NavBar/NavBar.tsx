ecf7b6c93fe3e60449acb13577d79c8a6bcf633f
Eloy <eloy.robillard@gmail.com>
2021-11-28 10:58:53 +0100

    [Client | NavBar] refactor dropdown style

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -4,28 +4,31 @@ import './NavBar.css';
 export default function NavBar (): JSX.Element {
 	return (
 		<div className='navbar'>
-			<ul className='nav-select'>
-				<li>
-					<div>Navigation</div>
-					<ul>
-						<li>
-							<Link to='/profile'>Profile</Link>
-						</li>
-						<li>
-							<Link to='/trips'>Trips</Link>
-						</li>
-						<li>
-							<Link to='/journal'>Journal</Link>
-						</li>
-						<li>
-							<Link to='/notes'>Notes</Link>
-						</li>
-						<li>
-							<Link to='/logout'>Logout</Link>
-						</li>
-					</ul>
-				</li>
-			</ul>
+			<img className='navbar-circle' />
+			<div className='nav-select'>
+				<ul className='nav-select__list'>
+					<li>
+						<div>Navigation</div>
+						<ul>
+							<li>
+								<Link to='/profile'>Profile</Link>
+							</li>
+							<li>
+								<Link to='/trips'>Trips</Link>
+							</li>
+							<li>
+								<Link to='/journal'>Journal</Link>
+							</li>
+							<li>
+								<Link to='/notes'>Notes</Link>
+							</li>
+							<li>
+								<Link to='/logout'>Logout</Link>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
