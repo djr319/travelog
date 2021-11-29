801d1691ccebde84f0f26c8cd62eca361f339fd3
David Ryan <mail@djryan.eu>
2021-11-29 10:02:02 +0100

    [App.js] Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 
-export default function App () {
-	return (
-		<div className='App'>
-			<NavBar />
-			{/* <Outlet /> */}
-		</div>
-	);
+export default function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
 }
