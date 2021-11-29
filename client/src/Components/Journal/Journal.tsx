0144064dbaf55afbcb3e1ea5aa5b1d2cad8ff87a
Eloy <eloy.robillard@gmail.com>
2021-11-29 14:55:29 +0100

    [Client | Journal] add side menu

--- client/src/Components/Journal/Journal.tsx
+++ client/src/Components/Journal/Journal.tsx
@@ -1,6 +1,7 @@
 import { JournalAPI } from 'Services/index';
 import { useState } from 'react';
 import './Journal.css';
+import Arrow from 'Assets/arrow.svg';
 
 const MIN_LEN = 10;
 const MAX_LEN = 30;
@@ -8,9 +9,17 @@ const MAX_LEN = 30;
 export default function Journal (): JSX.Element {
 	const [ review, setReview ] = useState('');
 
+  const [ menuPos, setMenuPos ] = useState({ left: -105 });
+  const [ arrowRot, setArrowRot ] = useState(180);
+
 	async function handleSubmit () {
 		JournalAPI.addJournal({ review });
 	}
+
+  function toggleMenu () {
+    setMenuPos((prev) => { return { left: -prev.left }});
+    setArrowRot((prev) => 180 - prev);
+  }
 
 	function onChange (e: React.FormEvent<HTMLTextAreaElement>) {
 		setReview(e.currentTarget.value);
@@ -18,6 +27,16 @@ export default function Journal (): JSX.Element {
 
 	return (
 		<div className='journal'>
+			<div className='journal__menu' style={menuPos}>
+				<img className='journal__menu-button' src={Arrow} onClick={toggleMenu} style={{transform: `rotate(${arrowRot}deg)`}}/>
+				<div className='journal__menu-select-container'>
+					<div className='journal__menu-select'>
+						<div className='journal__menu-select-entry'>Journal 1</div>
+						<div className='journal__menu-select-entry'>Journal 2</div>
+					</div>
+					<div />
+				</div>
+			</div>
 			<form className='journal__form' onSubmit={handleSubmit}>
 				<div className='journal__form-textarea-container'>
 					<textarea
@@ -30,9 +49,13 @@ export default function Journal (): JSX.Element {
 						value={review}
 						onInput={onChange}
 					/>
-          {review.length < MIN_LEN ? 'Insufficient characters.' : `${review.length}/${MAX_LEN} characters.`}
+					{review.length < MIN_LEN ? (
+						'Insufficient characters.'
+					) : (
+						`${review.length}/${MAX_LEN} characters.`
+					)}
 				</div>
-				<button type='submit'>Save Journal</button>
+				<button type='submit'>Add story</button>
 			</form>
 		</div>
 	);
