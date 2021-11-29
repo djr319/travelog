f3279e168a3aa5416e384438df5b100e39339bdd
Eloy <eloy.robillard@gmail.com>
2021-11-29 15:12:17 +0100

    [Client | Journal] fix menu move range

--- client/src/Components/Journal/Journal.tsx
+++ client/src/Components/Journal/Journal.tsx
@@ -9,17 +9,22 @@ const MAX_LEN = 30;
 export default function Journal (): JSX.Element {
 	const [ review, setReview ] = useState('');
 
-  const [ menuPos, setMenuPos ] = useState({ left: -105 });
-  const [ arrowRot, setArrowRot ] = useState(180);
+	const [ menuPos, setMenuPos ] = useState(-105);
+	const [ arrowRot, setArrowRot ] = useState(180);
 
 	async function handleSubmit () {
 		JournalAPI.addJournal({ review });
 	}
 
-  function toggleMenu () {
-    setMenuPos((prev) => { return { left: -prev.left }});
-    setArrowRot((prev) => 180 - prev);
-  }
+	function toggleMenu () {
+		setMenuPos((prev) => {
+      if (prev >= 0) {
+        return -105;
+      }
+      return 0;
+    });
+		setArrowRot((prev) => 180 - prev);
+	}
 
 	function onChange (e: React.FormEvent<HTMLTextAreaElement>) {
 		setReview(e.currentTarget.value);
@@ -27,8 +32,13 @@ export default function Journal (): JSX.Element {
 
 	return (
 		<div className='journal'>
-			<div className='journal__menu' style={menuPos}>
-				<img className='journal__menu-button' src={Arrow} onClick={toggleMenu} style={{transform: `rotate(${arrowRot}deg)`}}/>
+			<div className='journal__menu' style={{ left: menuPos }}>
+				<img
+					className='journal__menu-button'
+					src={Arrow}
+					onClick={toggleMenu}
+					style={{ transform: `rotate(${arrowRot}deg)` }}
+				/>
 				<div className='journal__menu-select-container'>
 					<div className='journal__menu-select'>
 						<div className='journal__menu-select-entry'>Journal 1</div>
