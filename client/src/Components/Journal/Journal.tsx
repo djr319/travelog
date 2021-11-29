f5e58a25f98ce7ffc3174557e7b9db7c703ae572
Eloy <eloy.robillard@gmail.com>
2021-11-29 12:02:54 +0100

    [Client] npm registry clean-up

--- client/src/Components/Journal/Journal.tsx
+++ client/src/Components/Journal/Journal.tsx
@@ -1,15 +1,13 @@
-
-
 export default function Journal (): JSX.Element {
-  function handleSubmit () {
-    return 0;
-  }
+	function handleSubmit () {
+		return 0;
+	}
 
-  return (
-    <div className="journal">
-      <form className="journal__form" onSubmit={handleSubmit}>
-        <button type='submit'>Save Journal</button>
-      </form>
-    </div>
-  )
+	return (
+		<div className='journal'>
+			<form className='journal__form' onSubmit={handleSubmit}>
+				<button type='submit'>Save Journal</button>
+			</form>
+		</div>
+	);
 }
