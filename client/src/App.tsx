8adf81895bf0d05ec28e3401bed7ad30f636d804
Eloy <eloy.robillard@gmail.com>
2021-11-29 10:07:27 +0100

    [Client | App] re-insert Outlet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import NavBar from 'Components/NavBar/NavBar';
 // import React from 'react';
-// import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 export default function App (): JSX.Element {
