16e88aefe4b7ab2015095d826925fb753a62d81c
Eloy <eloy.robillard@gmail.com>
2021-11-29 18:38:40 +0100

    [Client | Components] export all comps in index

--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -1,3 +1,5 @@
 // LINK https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
 export { default as Journal } from './Journal/Journal';
+export { default as NavBar } from './NavBar/NavBar';
 export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as TripsForm } from './TripsForm/TripsForm';
