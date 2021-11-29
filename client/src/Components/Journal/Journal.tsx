2fdab389c208560e8bc4a9c16a472077a49da9e8
Eloy <eloy.robillard@gmail.com>
2021-11-29 12:35:40 +0100

    [Client | Journal] style text area

--- client/src/Components/Journal/Journal.tsx
+++ client/src/Components/Journal/Journal.tsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
 import { JournalAPI } from 'Services/index';
+import { useState } from 'react';
+import './Journal.css';
+
+const MAX_LEN = 30;
 
 export default function Journal (): JSX.Element {
 	const [ review, setReview ] = useState('');
@@ -15,7 +18,15 @@ export default function Journal (): JSX.Element {
 	return (
 		<div className='journal'>
 			<form className='journal__form' onSubmit={handleSubmit}>
-				<textarea name='review' value={review} onInput={onChange} />
+				<textarea
+					className='journal__form-textarea'
+          placeholder='Enter review description...'
+          required={true}
+					maxLength={MAX_LEN}
+					name='review'
+					value={review}
+					onInput={onChange}
+				/>
 				<button type='submit'>Save Journal</button>
 			</form>
 		</div>
