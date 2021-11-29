37d5a4e28332a6a0caaa2198b1f5bd91dceccc7c
Eloy <eloy.robillard@gmail.com>
2021-11-29 12:27:42 +0100

    [Client | Journal] set up text area & update

--- client/src/Components/Journal/Journal.tsx
+++ client/src/Components/Journal/Journal.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+import { JournalAPI } from 'Services/index';
+
 export default function Journal (): JSX.Element {
-	function handleSubmit () {
-		return 0;
+	const [ review, setReview ] = useState('');
+
+	async function handleSubmit () {
+		JournalAPI.addJournal({ review });
+	}
+
+	function onChange (e: React.FormEvent<HTMLTextAreaElement>) {
+		setReview(e.currentTarget.value);
 	}
 
 	return (
 		<div className='journal'>
 			<form className='journal__form' onSubmit={handleSubmit}>
+				<textarea name='review' value={review} onInput={onChange} />
 				<button type='submit'>Save Journal</button>
 			</form>
 		</div>
