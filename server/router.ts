45e9bad2c9b28927d78c6c0e871dc3c5d3fafa6e
Eloy <eloy.robillard@gmail.com>
2021-11-30 17:15:51 +0100

    [Server] shorten query names

--- server/router.ts
+++ server/router.ts
@@ -3,26 +3,26 @@ import express from 'express';
 const router = express.Router();
 const trip = require('./controllers/controller');
 
-router.post('/signup', trip.doSignUp);
+router.post('/signup', trip.signUp);
 router.get('/login', trip.getLogin);
 router.get('/profile', trip.getProfile);
 
-router.post('/trips', trip.addNewTrip);
-router.get('/trips', trip.getAllPersonalTrips);
-router.get('/trips/:id', trip.getOnePersonalTrip);
-// router.put('/trips/:id', trip.updateOnePersonalTrip);
-// router.delete('/trips/:id', trip.deleteOnePersonalTrip);
+router.post('/trips', trip.addTrip);
+router.get('/trips', trip.getOwnTrips);
+router.get('/trips/:id', trip.getTrip);
+// router.put('/trips/:id', trip.updateTrip);
+// router.delete('/trips/:id', trip.deleteTrip);
 
-router.post('/journals', trip.addNewJournal);
-router.get('/journals', trip.getAllPersonalJournals);
-router.get('/journals/:id', trip.getOnePersonalJournal);
+router.post('/journals', trip.addJournal);
+router.get('/journals', trip.getOwnJournals);
+router.get('/journals/:id', trip.getJournal);
 router.get('/journals/collections', trip.getAllPublicJournals);
-// router.put('/journals/:id', trip.updateOnePersonalJournal);
-// router.delete('/journals/:id', trip.deleteOnePersonalJournal);
+// router.put('/journals/:id', trip.updateJournal);
+// router.delete('/journals/:id', trip.deleteJournal);
 
 router.post('/notes', trip.addNewNote);
-router.get('/notes', trip.getAllPersonalNotes);
-// router.delete('/notes/:id', trip.deleteOnePersonalNote);
+router.get('/notes', trip.getOwnNotes);
+// router.delete('/notes/:id', trip.deleteNote);
 
 
 export default router;
