2f93ef0d294ff79cb817660e51ced3db40d6fe24
Eloy <eloy.robillard@gmail.com>
2021-12-01 18:07:57 +0100

    [Client|Services] fix importing fetchRequest as default

--- client/src/Services/index.ts
+++ client/src/Services/index.ts
@@ -4,7 +4,7 @@ export { default as FirebaseAPI } from './firebase.service';
 
 const BASE_URL = 'http://localhost:3001'
 
-export default async function fetchRequest<T>(url: string, options?: RequestInit): Promise<T> {
+export async function fetchRequest<T>(url: string, options?: RequestInit): Promise<T> {
   return await fetch(`${BASE_URL}${url}`, options)
     .then(response => response.json())
     .catch((err) => console.log(err));
--- client/src/Services/trips.service.ts
+++ client/src/Services/trips.service.ts
@@ -1,4 +1,4 @@
-import fetchRequest from "./index";
+import { fetchRequest } from "./index";
 import { Trip } from 'Types';
 
 const TRIPS_URL = "/trips";
