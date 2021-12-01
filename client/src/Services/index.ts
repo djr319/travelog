20baa584b4602565c64c0c631d6a6694c7e33ad7
Eloy <eloy.robillard@gmail.com>
2021-12-01 17:46:49 +0100

    [Client|Types] remove 'any' type

--- client/src/Services/index.ts
+++ client/src/Services/index.ts
@@ -1,8 +1,12 @@
+import { Journal, Trip } from 'Types';
 export { default as JournalAPI } from './journal.service';
+export { default as TripsAPI } from './trips.service';
 
 const BASE_URL = 'http://localhost:3001'
 
-export default async function fetchRequest(url: string, options?: RequestInit): Promise<any> {
+type AnyVal = Journal | Journal[] | Trip | Trip[];
+
+export default async function fetchRequest(url: string, options?: RequestInit): Promise<AnyVal> {
   return await fetch(`${BASE_URL}${url}`, options)
     .then(response => response.json())
     .catch((err) => console.log(err));
