09d4355e3a27a8da30ba60e07e3da8ebc1c665f2
urisk <carst333@gmail.com>
2021-12-02 15:50:40 +0100

    [FRONTEND: CLIENT]: Add npm packages

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,16 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import router from './router';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import path from 'path';
 
 const app: Application = express();
 const PORT = 3001;
+const httpServer = createServer(app);
+const io = new Server(httpServer,
+  path = 
+)
 
 app.use(express.json());
 app.use(cors());
