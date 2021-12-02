6439d660e2614188d68af066dc9bb24e86915a6d
David Ryan <mail@djryan.eu>
2021-12-02 23:10:30 +0100

    [STYLE]Navbar style

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import { GrHome, GrUser, GrMapLocation, GrBook, GrNotes, GrLogout } from "react-icons/gr";
-import { ThemeProvider } from 'styled-components';
-import { base, deepMerge } from 'grommet-icons';
+import { GrUser, GrMapLocation, GrBook, GrNotes, GrLogout } from "react-icons/gr";
+// import { ThemeProvider } from 'styled-components';
+// import { base, deepMerge } from 'grommet-icons';
 import logo from 'Assets/logo.jpg';
 import "./NavBar.css";
 // import Login from '../../Components/LoginButton/LoginButton';
@@ -60,46 +60,46 @@ import "./NavBar.css";
 
 // https://react-icons.github.io/react-icons/icons?name=gr
 
-const theme = deepMerge(base, {
-  global: {
-    colors: {
-      brand: '#ff0000',
-    },
-  },
-});
+// const theme = deepMerge(base, {
+//   global: {
+//     colors: {
+//       brand: '#ff0000',
+//     },
+//   },
+// });
 
 
 export default function NavBar(): JSX.Element {
   return (
-    <ThemeContext.Provider theme={theme}>
-      <div className="navbar">
-        {/* <div className="navbar-circle">
+    // <ThemeContext.Provider theme={theme}>
+    <div className="navbar">
+      {/* <div className="navbar-circle">
 
       </div> */}
-        <Link to="/">
-          <img src={logo} className="logo" alt="Travelog logo" />
-        </Link>
-        <div className="nav-select">
-          <ul className="nav-select__list">
-            <li>
-              <Link to='/profile'><GrUser /></Link>
-            </li>
-            <li>
-              <Link to="/trips"><GrMapLocation /></Link>
-            </li>
-            <li>
-              <Link to="/journal"><GrBook /></Link>
-            </li>
-            <li>
-              <Link to='/notes'><GrNotes /></Link>
-            </li>
-            <li>
-              <GrLogout />
-            </li>
-          </ul>
-        </div >
+      <Link to="/">
+        <img src={logo} className="logo" alt="Travelog logo" />
+      </Link>
+      <div className="nav-select">
+        <ul className="nav-select__list">
+          <li>
+            <Link to='/profile'><GrUser /></Link>
+          </li>
+          <li>
+            <Link to="/trips"><GrMapLocation /></Link>
+          </li>
+          <li>
+            <Link to="/journal"><GrBook /></Link>
+          </li>
+          <li>
+            <Link to='/notes'><GrNotes /></Link>
+          </li>
+          <li>
+            <GrLogout />
+          </li>
+        </ul>
       </div >
-    </ThemeContext.Provider>
+    </div >
+    // </ThemeContext.Provider>
   );
 }
 
