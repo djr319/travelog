5fc9892f44617ae641654aa6043292caa1460053
natas <natasa410@hotmail.com>
2021-12-02 14:10:37 +0000

    [PROFILE]:profile form and view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Dashboard, Journal, TripsForm, NavBar, Notes } from "Components";
-import { Note } from 'Types/index';
-import { NoteAPI } from 'Services/index';
-import { NoteContext, NotesContext } from './Context/Context';
+import { Note } from "Types/index";
+import { NoteAPI } from "Services/index";
+import { NoteContext, NotesContext } from "./Context/Context";
 // import Notes from './Components/Notes/Notes';
 import { UserProvider } from "Context";
 import "firebase/compat/auth";
@@ -13,7 +13,6 @@ import { FirebaseAPI } from "Services";
 import { StyledFirebaseAuth } from "react-firebaseui";
 
 export default function App(): JSX.Element {
-
   const { auth, uiConfig } = FirebaseAPI.getConfig();
 
   const [notes, setNotes] = useState<Note[]>([]);
@@ -49,30 +48,33 @@ export default function App(): JSX.Element {
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
 
-
   useEffect(() => {
     (async () => {
       const notes = await NoteAPI.getPersonalNotes(id);
-      const sortedNotes = notes.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+      const sortedNotes = notes.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
       setNotes(sortedNotes);
     })();
-	}, []);
-  
-  function addNote (note: Note): void {
-    NoteAPI.addNote(note)
-      .then(newNote => setNotes([...notes, newNote]
-        .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-      ));
+  }, []);
+
+  function addNote(note: Note): void {
+    NoteAPI.addNote(note).then((newNote) =>
+      setNotes(
+        [...notes, newNote].sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        )
+      )
+    );
   }
 
-  async function deleteNote (id: number): Promise<void> {
+  async function deleteNote(id: number): Promise<void> {
     await NoteAPI.deleteNote(id);
-    const filteredNotes = notes.filter(note => note.id !== id);
+    const filteredNotes = notes.filter((note) => note.id !== id);
     setNotes(filteredNotes);
   }
-
-
-  
 
   if (!isSignedIn) {
     return (
@@ -89,42 +91,42 @@ export default function App(): JSX.Element {
     <div>
       <UserProvider value={user}>
         <a onClick={() => auth.signOut()}>Sign-out</a>
-      <NoteContext.Provider value={{deleteNote, addNote}} >
-      <NotesContext.Provider value={notes} > 
-        <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route
-              path="/trips"
-              element={<ListOfTrips trips={trips} setTrips={SetTrips} />}
-            />
-            <Route path="/trips" element={<TripsForm />} />
-            <Route path="/trip" element={<ViewPersonalTrip />} />
-            <Route path="/trip/:id" element={<ViewPersonalTrip />} />
-            {/*
-          <Route path="/profile" element={<Dashboard />} />
+        <NoteContext.Provider value={{ deleteNote, addNote }}>
+          <NotesContext.Provider value={notes}>
+            <BrowserRouter>
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route
+                  path="/trips"
+                  element={<ListOfTrips trips={trips} setTrips={SetTrips} />}
+                />
+                <Route path="/form" element={<TripsForm />} />
+                <Route path="/trip" element={<ViewPersonalTrip />} />
+                <Route path="/trip/:id" element={<ViewPersonalTrip />} />
+                <Route path="/profile" element={<Dashboard />} />
+                {/*
           <Route path="/planning" element={<Dashboard />} />
           <Route path="/route" element={<Dashboard />} />
           <Route path="/weather" element={<Dashboard />} />
           <Route path="/logout" element={<Dashboard />} />
         */}
-            <Route path='journal' element={<Journal />} />
-            <Route path="/notes" element={<Notes />} />
-            <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>We've wandered off the beaten track. Nothing here!</p>
-                  <p>{"User: " + auth.currentUser?.displayName}</p>
-                </main>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </NotesContext.Provider>
-      </NoteContext.Provider>
-      </UserProvider >
-    </div >
+                <Route path="journal" element={<Journal />} />
+                <Route path="/notes" element={<Notes />} />
+                <Route
+                  path="*"
+                  element={
+                    <main style={{ padding: "1rem" }}>
+                      <p>We've wandered off the beaten track. Nothing here!</p>
+                      <p>{"User: " + auth.currentUser?.displayName}</p>
+                    </main>
+                  }
+                />
+              </Routes>
+            </BrowserRouter>
+          </NotesContext.Provider>
+        </NoteContext.Provider>
+      </UserProvider>
+    </div>
   );
 }
--- client/src/Services/index.ts
+++ client/src/Services/index.ts
@@ -1,12 +1,16 @@
-export { default as JournalAPI } from './journal.service';
-export { default as NoteAPI} from './note.service';
-export { default as TripsAPI } from './trips.service';
-export { default as FirebaseAPI } from './firebase.service';
+export { default as JournalAPI } from "./journal.service";
+export { default as NoteAPI } from "./note.service";
+export { default as TripsAPI } from "./trips.service";
+export { default as ProfileAPI } from "./profile.service";
+export { default as FirebaseAPI } from "./firebase.service";
 
-const BASE_URL = 'http://localhost:3001'
+const BASE_URL = "http://localhost:3001";
 
-export async function fetchRequest<T>(url: string, options?: RequestInit): Promise<T> {
+export async function fetchRequest<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
   return await fetch(`${BASE_URL}${url}`, options)
-    .then(response => response.json())
+    .then((response) => response.json())
     .catch((err) => console.log(err));
 }
